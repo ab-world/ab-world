45f18cad6ab3925f02b8e31ea2dcc708ffc9847e
HYUNSOOSHIN <shs0655@gmail.com>
2025-12-02 19:36:21 +0900

    feat: 산업 페이지 - 신규 개발

---
+++ src/app/(pages)/(industry)/industry-03/page.jsx
@@ -0,0 +1,24 @@
+// 산업 페이지
+'use client';
+import styles from './page.module.scss';
+import { useEffect, useRef } from 'react';
+
+export default function Industry(props) {
+    const divRef = useRef(null);
+
+    useEffect(() => {
+        if (divRef.current) {
+            fetch('/industry-03.html')
+                .then((res) => res.text())
+                .then((text) => (divRef.current.innerHTML = text));
+        }
+    }, [divRef]);
+
+    return (
+        <div className={styles.page}>
+            <main className={styles.main}>
+                <div ref={divRef} />
+            </main>
+        </div>
+    );
+}
