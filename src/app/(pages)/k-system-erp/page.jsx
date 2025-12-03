6acf1746b0d4d1f6869b50ae6e27ddefa93528c6
HYUNSOOSHIN <shs0655@gmail.com>
2025-12-03 23:17:48 +0900

    feat: 보완사항 작업 3

--- src/app/(pages)/k-system-erp/page.jsx
+++ src/app/(pages)/k-system-erp/page.jsx
@@ -10,7 +10,54 @@ export default function KSystemERP(props) {
         if (divRef.current) {
             fetch('/k-system-erp.html')
                 .then((res) => res.text())
-                .then((text) => (divRef.current.innerHTML = text));
+                .then((text) => {
+                    divRef.current.innerHTML = text;
+
+                    const accordions = divRef.current.querySelectorAll('.accordion-item');
+
+                    accordions.forEach((item) => {
+                        const header = item.querySelector('.accordion-title a');
+                        const content = item.querySelector('.accordion-collapse');
+
+                        content.style.overflow = 'hidden';
+                        content.style.height = '0';
+                        content.style.transition = `max-height 0.5s ease-in-out`;
+
+                        header.addEventListener('click', (e) => {
+                            e.preventDefault();
+
+                            const isOpen = item.classList.contains('active');
+
+                            if (isOpen) {
+                                item.classList.remove('active');
+                                header.classList.add('collapsed');
+                                content.style.height = '0';
+                                content.classList.remove('in');
+                            } else {
+                                accordions.forEach((otherItem) => {
+                                    const otherHeader = otherItem.querySelector('.accordion-title a');
+                                    const otherContent = otherItem.querySelector('.accordion-collapse');
+
+                                    otherContent.style.transition = `height 0.5s ease-in-out`;
+
+                                    if (otherContent !== content) {
+                                        otherItem.classList.remove('active');
+                                        otherHeader.classList.add('collapsed');
+
+                                        otherContent.style.height = '0';
+                                        otherContent.classList.remove('in');
+                                    }
+                                });
+
+                                // 현재 아코디언 열기
+                                item.classList.add('active');
+                                header.classList.remove('collapsed');
+                                content.style.height = content.scrollHeight + 'px';
+                                content.classList.add('in');
+                            }
+                        });
+                    });
+                });
         }
     }, [divRef]);
 
