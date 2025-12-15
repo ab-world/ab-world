5705599e739c9a25ac202ca61f9d80e1a0c879f1
HYUNSOOSHIN <shs0655@gmail.com>
2025-12-15 22:02:38 +0900

    feat: favicon png 이미지 파일 추가

--- src/util/seo.js
+++ src/util/seo.js
@@ -18,9 +18,10 @@ export const getMetadata = (metadataProps) => {
         keywords: [...META.keyword],
         icons: {
             icon: [
-                // { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-                // { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-                // { url: '/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
+                { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+                { url: '/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
+                { url: '/favicon-64x64.png', sizes: '64x64', type: 'image/png' },
+                { url: '/favicon-128x128.png', sizes: '128x128', type: 'image/png' },
                 { url: '/favicon.ico', sizes: 'any' }
             ]
         },
